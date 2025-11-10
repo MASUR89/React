94cf665274e3ece80e0cb5e020114cd207394ef5
MASUR89 <surmava9@gmail.com>
2025-11-10 21:20:58 +0400

    useRef

--- lessons/src/components/LessonTS.tsx
+++ lessons/src/components/LessonTS.tsx
@@ -1,24 +1,53 @@
 import React, { useRef, useState, useEffect } from 'react'
 
-//typescript lesson component
-
 function LessonTS() {
-  const [count, setCount] = useState(100);
-  const prevCountRef = useRef(100);
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
+  const [cvc, setCvc] = useState("");
+  
+  const nameRef = useRef("");
+  const numberRef = useRef("");
+  const monthRef = useRef("");
+  const yearRef = useRef("");
+  const cvcRef = useRef("");
 
-  useEffect(() => {
-    prevCountRef.current = count;
-  }, [count]);
-
-  const handleClick = () => {
-    setCount((prev) => prev - 2);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    nameRef.current = e.target.value;
+    numberRef.current = e.target.value;
+    monthRef.current = e.target.value;
+    yearRef.current = e.target.value;
+    cvcRef.current = e.target.value;
+    
   };
 
+  function handleSubmit() {
+    setName(nameRef.current);
+    setNumber(numberRef.current);
+    setMonth(monthRef.current);
+    setYear(yearRef.current);
+    setCvc(cvcRef.current);
+  }
+
+  useEffect(() => {
+  
+  }, []);
+
   return (
-    <div >
-      <h2>Current Count: {count}</h2>
-      <h2>Previous Count: {prevCountRef.current ?? "—"}</h2>
-      <button onClick={handleClick}>Decrease by 2</button>
+    <div>
+      <h2>track data</h2>
+      <input type="text" onChange={handleChange} placeholder='name' />
+      <input type="text" onChange={handleChange} placeholder='number' />
+      <input type="text" onChange={handleChange} placeholder='month' />
+      <input type="text" onChange={handleChange} placeholder='year' />
+      <input type="text" onChange={handleChange} placeholder='cvc' />
+      <button onClick={handleSubmit}>Show text</button>
+      <p>Name: {name}</p>
+      <p>Number: {number}</p>
+      <p>Month: {month}</p>
+      <p>Year: {year}</p>
+      <p>CVC: {cvc}</p>
     </div>
   );
 }
